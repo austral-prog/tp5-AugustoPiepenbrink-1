5600b04ad1036d0e80dba281158f56bfc792cc18
AugustoPiepenbrink <apiepenbrink@mail.austral.edu.ar>
2025-09-26 09:50:19 -0300

    Update number_to_month.py

--- number_to_month.py
+++ number_to_month.py
@@ -1,4 +1,16 @@
-# Replace the "ANSWER HERE" for your answer
-
 def number_to_month(month):
-    return "ANSWER HERE" # Remove this line and implement
+
+	meses = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"]
+	
+	if 1 <= month <= 12:
+		
+		return meses [month - 1]
+
+	else:
+		
+		return "error"
+	
+numero_de_mes = int(input("Ingresá un número del 1 al 12 para saber el mes:"))
+
+mes = number_to_month(numero_de_mes)
+print(f"El mes es: {mes}")
