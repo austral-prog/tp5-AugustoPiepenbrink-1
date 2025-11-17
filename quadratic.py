4b15b4ca7092a633199903c77a7babe760571330
AugustoPiepenbrink <apiepenbrink@mail.austral.edu.ar>
2025-11-17 20:46:57 -0300

    Implement quadratic functions for roots and calculations

--- quadratic.py
+++ quadratic.py
@@ -1,16 +1,22 @@
-# Replace the "ANSWER HERE" for your answer
-
 def roots(a, b, c):
-    return "ANSWER HERE"
+    
+    discriminante = b**2 - 4*a*c
 
+    if discriminante > 0:
+        r1 = (-b + (discriminante)**0.5) / (2*a)
+        r2 = (-b - (discriminante)**0.5) / (2*a)
+        return f"({r1}, {r2})"
+    elif discriminante == 0:
+        r = -b / (2*a)
+        return f"({r})"
+    else:
+        return "( )"
 
 def value_y(a, b, c, x):
-    return "ANSWER HERE"
-
+    return a*x**2 + b*x + c
 
 def to_string(a, b, c):
-    return "ANSWER HERE"
-
+    return f"f(x) = {a} * X^2 + {b} * X + {c}"
 
 def derivation(a, b, c):
-    return "ANSWER HERE"
+    return f"f'(x) = {2*a}x + {b}"
