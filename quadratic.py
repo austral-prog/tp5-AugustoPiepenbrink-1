9b388bc4283fc490a6fed07cf079a36ff88303b8
AugustoPiepenbrink <apiepenbrink@mail.austral.edu.ar>
2025-11-19 00:28:48 -0300

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -1,22 +1,23 @@
 def roots(a, b, c):
-    
     discriminante = b**2 - 4*a*c
 
     if discriminante > 0:
         r1 = (-b + (discriminante)**0.5) / (2*a)
         r2 = (-b - (discriminante)**0.5) / (2*a)
-        return f"({r1}, {r2})"
+        return (r1, r2)
+
     elif discriminante == 0:
         r = -b / (2*a)
-        return f"({r})"
+        return (r,)
+
     else:
-        return "( )"
+        return ()
 
 def value_y(a, b, c, x):
     return a*x**2 + b*x + c
 
 def to_string(a, b, c):
-    return f"f(x) = {a} * X^2 + {b} * X + {c}"
+    return f"f(x) = {a}*x^2 + {b}*x + {c}"
 
 def derivation(a, b, c):
-    return f"f'(x) = {2*a}x + {b}"
+    return f"f'(x) = {2*a}*x + {b}"
