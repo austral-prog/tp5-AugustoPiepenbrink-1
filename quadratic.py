89908f1dee82e8400b9305b117ebcc1ca5bc05c7
AugustoPiepenbrink <apiepenbrink@mail.austral.edu.ar>
2025-11-19 00:33:09 -0300

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -1,23 +1,41 @@
+import math
+
 def roots(a, b, c):
     discriminante = b**2 - 4*a*c
-
     if discriminante > 0:
-        r1 = (-b + (discriminante)**0.5) / (2*a)
-        r2 = (-b - (discriminante)**0.5) / (2*a)
-        return (r1, r2)
-
+        x1 = (-b + math.sqrt(discriminante)) / (2*a)
+        x2 = (-b - math.sqrt(discriminante)) / (2*a)
+        return f"({x1}, {x2})"
     elif discriminante == 0:
-        r = -b / (2*a)
-        return (r,)
-
+        x = -b / (2*a)
+        return f"({x})"
     else:
-        return ()
+        return "( )"
+
 
 def value_y(a, b, c, x):
     return a*x**2 + b*x + c
 
+
 def to_string(a, b, c):
-    return f"f(x) = {a}*x^2 + {b}*x + {c}"
+    if a != 0 and b != 0:
+        return f"f(x) = {a} * X^2 + {b} * X + {c}"
+    elif a == 0 and b != 0:
+        return f"f(x) = {b} * X + {c}"
+    elif a != 0 and b == 0:
+        return f"f(x) = {a} * X^2 + {c}"
+    else:
+        return f"f(x) = {c}"
+
 
 def derivation(a, b, c):
-    return f"f'(x) = {2*a}*x + {b}"
+    da = 2*a
+    db = b
+    if da != 0 and db != 0:
+        return f"f'(x) = {da} * X + {db}"
+    elif da == 0 and db != 0:
+        return f"f'(x) = {db}"
+    elif da != 0 and db == 0:
+        return f"f'(x) = {da} * X"
+    else:
+        return "f'(x) = 0"
